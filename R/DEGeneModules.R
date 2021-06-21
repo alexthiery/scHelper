75c861b96538fc2c2232e8d892ab788c543600df
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-06-21 18:57:48 +0100

    add new function for calculating differentially expressed gene modules

---
+++ R/DEGeneModules.R
@@ -0,0 +1,15 @@
+#' Filter gene modules based on differential expression (seurat::FindAllMarkers)
+#'
+#' @param seurat_object Seurat object
+#' @param gene_modules named list of gene modules
+#' @param logfc logFC cutoff used for differential expression
+#' @param pval adjusted pvalue
+#' @param selected_gene_proportion proportion of genes in gene module which are required to pass differential expression test
+#' @return gene module list
+#' @export
+DEGeneModules <- function(seurat_data, gene_modules, logfc = 0.25, pval = 0.001, selected_gene_proportion = 0.5, active_ident = NULL){
+  if(!is.null(active_ident)){Idents(object = seurat_data) <- active_ident}
+  DE_genes <- FindAllMarkers(seurat_data, only.pos = T, logfc.threshold = logfc) %>% filter(p_val_adj < pval)
+  gms <- SubsetGeneModules(antler_data$gene_modules$get("unbiasedGMs"), selected_genes = DE_genes$gene, keep_mod_ID = T, selected_gene_proportion = selected_gene_proportion)
+  return(gms)
+}
--- R/SubsetGeneModules.R
+++ R/SubsetGeneModules.R
@@ -3,16 +3,16 @@
 #' @param gms List of gene modules obtained from Antler
 #' @param selected_genes array of genes used to filter gene module list
 #' @param keep_mod_ID boolean value for whether to keep original gene module ID
-#' @param selected_gene_ratio ratio of selected genes which must be present in order for the module to be kept
+#' @param selected_gene_proportion ratio of selected genes which must be present in order for the module to be kept
 #' @export
-SubsetGeneModules <- function(gms, selected_genes, keep_mod_ID = FALSE, selected_gene_ratio = 0) {
+SubsetGeneModules <- function(gms, selected_genes, keep_mod_ID = FALSE, selected_gene_proportion = 0) {
   outlist <- list()
   if(is.null(names(gms))){
     names(gms) <- paste0("GM: ", 1:length(gms))
   } else {}
   
   # this filters gene modules for which the percentage of differentially expressed genes passes a threshold test
-  gms = gms[unlist(lapply(gms, function(x) sum(x %in% selected_genes) >= round(length(x)*selected_gene_ratio)))]
+  gms = gms[unlist(lapply(gms, function(x) sum(x %in% selected_genes) >= round(length(x)*selected_gene_proportion)))]
   
   for(mod in names(gms)){
     if(any(selected_genes %in% gms[[mod]])){
