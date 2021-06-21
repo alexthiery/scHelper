1a784d9579671aa5a6b3c30428bda955916787de
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-06-21 17:20:47 +0100

    update averagegenemodules function

--- R/AverageGeneModules.R
+++ R/AverageGeneModules.R
@@ -10,12 +10,12 @@ AverageGeneModules <- function(seurat_obj, gene_list){
   if(is.null(names(gene_list))){stop('Gene list must contain named arrays')}
   
   # Override previous meta.data columns if present
-  if(any(colnames(seurat_obj@meta.data) %in% names(genes))){
-    seurat_obj@meta.data[, names(genes)] <- NULL
+  if(any(colnames(seurat_obj@meta.data) %in% names(gene_list))){
+    seurat_obj@meta.data[, names(gene_list)] <- NULL
   }
   
   seurat_obj <- AddModuleScore(seurat_obj, features = gene_list, name = 'genes')
-  colnames(seurat_obj@meta.data) <- replace(colnames(seurat_obj@meta.data), grepl('genes', colnames(seurat_obj@meta.data)), names(genes))
+  colnames(seurat_obj@meta.data) <- replace(colnames(seurat_obj@meta.data), grepl('genes', colnames(seurat_obj@meta.data)), names(gene_list))
   return(seurat_obj)
 }
 
