c158d62a250e9fec41d4b0f6a1bd5ad79c7149f0
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-07-21 16:48:06 +0100

    update classification function. log intermediate plots

--- R/ClusterClassification.R
+++ R/ClusterClassification.R
@@ -10,88 +10,94 @@
 #' @return array of clusters which are outliers
 #' @export
 
-ClusterClassification <- function (seurat_obj, group_by = "seurat_clusters", metrics, 
-                  quantile, annotation_name = "scHelper_cell_type") 
+ClusterClassification <- function (seurat_obj, group_by = "seurat_clusters", cell_type_markers, 
+                                   quantile, annotation_name = "scHelper_cell_type", plot_path = "scHelper_log/", fast = FALSE, ...) 
 {
+  dir.create(plot_path, showWarnings = FALSE)
+  
   if (!length(quantile) == 1) {
     stop("quantile must be a single numeric value")
   }
-  if (!all(metrics %in% colnames(seurat_obj@meta.data))) {
-    missing_metrics <- paste(metrics[!metrics %in% colnames(seurat_obj@meta.data)], 
-                             collapse = ", ")
-    stop("The following cell types are missing from the seurat metadata: ", 
-         missing_metrics, ".\nPlease check that scHelper::AverageGeneModules has beeen ran for the missing cell types.")
-  }
-  
-  tic = TRUE
-  metadata <- seurat_obj@meta.data
+  # if (!all(metrics %in% colnames(seurat_obj@meta.data))) {
+  #   missing_metrics <- paste(metrics[!metrics %in% colnames(seurat_obj@meta.data)], 
+  #                            collapse = ", ")
+  #   stop("The following cell types are missing from the seurat metadata: ", 
+  #        missing_metrics, ".\nPlease check that scHelper::AverageGeneModules has beeen ran for the missing cell types.")
+  # }
+  # 
+  iterate = TRUE
+  iteration = 1
+  metrics = names(cell_type_markers)
+  temp_seurat <- seurat_obj
   cell_type_df = data.frame()
   
-  while(tic == TRUE){
+  if(fast == TRUE){
+    # Calculate average module expression for contamination gene list
+    temp_seurat <- AverageGeneModules(seurat_obj = temp_seurat, gene_list = cell_type_markers)
+  }
+  
+  while(iterate == TRUE){
+    if(fast == FALSE){
+      # Calculate average module expression for contamination gene list
+      temp_seurat <- AverageGeneModules(seurat_obj = temp_seurat, gene_list = cell_type_markers)
+    }
+    
     # select clusters and cell type annotations which are higher than percentile threshold
-    threshold_subset <- metadata %>%
-      dplyr::select(c(!!as.symbol(group_by), metrics)) %>%
+    threshold_subset <- temp_seurat@meta.data %>%
+      dplyr::select(c(!!as.symbol(group_by), all_of(metrics))) %>%
       reshape2::melt() %>%
       mutate(variable = as.character(variable)) %>%
       group_by(variable) %>%
-      mutate(threshold = quantile(value, probs = 0.9)) %>%
+      mutate(threshold = quantile(value, probs = quantile)) %>%
       group_by(!!as.symbol(group_by), variable) %>%
       mutate(median = median(value)) %>%
       filter(median >= threshold) %>%
       select(!value) %>%
       distinct(.keep_all = TRUE) %>%
       group_by(variable, .drop=FALSE) %>%
+      add_count(name="tally") %>%
+      group_by(!!as.symbol(group_by), .drop=FALSE) %>%
       add_count(name="tally")
+    
+    
+    temp_cell_type_df <- threshold_subset %>%
+      filter(tally == 1) %>%
+      dplyr::select(!!as.symbol(group_by), variable)
     
     # subset cell types which are assigned to only one cluster (remaining clusters will be re-assigned in loop)
     # if a remaining cluster has two cell-type identities, throw warning and assign to highest normalised median
-    unique_cell_type <- threshold_subset %>%
-      filter(tally == 1) %>%
+    temp_cell_type_df <- threshold_subset %>%
+      filter(tally != 1) %>%
       group_by(!!as.symbol(group_by)) %>%
       mutate(scaled_med = median/threshold) %>%
       top_n(1, scaled_med) %>%
-      dplyr::select(!!as.symbol(group_by), variable)
+      dplyr::select(!!as.symbol(group_by), variable) %>%
+      rbind(temp_cell_type_df)
     
-    tic = ifelse(all(threshold_subset$tally == 1), FALSE, TRUE)
     
-    # remove unique cell types from metadata in order to re-assign remaining cells
-    metadata <- filter(metadata, !(!!as.symbol(group_by) %in% unique_cell_type[[eval(group_by)]]))
+    png(paste0(plot_path, "classification_iteration_", iteration, ".png"), width = 40, height = 30, units = "cm", res = 200)
+    seurat_obj@misc[[annotation_name]] <- append(seurat_obj@misc[[annotation_name]], list( iteration = grid.arrange(grobs = PlotOutliers(seurat_obj = temp_seurat, 
+                                                                                                                                         y_elements = metrics, group_by = group_by, 
+                                                                                                                                         quantiles = quantile), ...)))
+    graphics.off()
+    
+    # remove assigned cells from seurat object in order to re-assign remaining cells
+    temp_seurat <- subset(temp_seurat, subset = !!as.symbol(group_by) %in% temp_cell_type_df[[group_by]], invert=TRUE)
+    
+    iteration = iteration + 1
+    iterate = ifelse(nrow(temp_cell_type_df) == 0, FALSE, TRUE)
     
     # iteratively add cluster assignments
-    cell_type_df <- rbind(cell_type_df, unique_cell_type)
+    cell_type_df <- rbind(cell_type_df, temp_cell_type_df)
   }
   
   if (nrow(cell_type_df) == 0) {
     stop("No cell types detected!")
   }
   
-  # NO LONGER USED
-  # duplicated_clusters <- cell_type_df[[group_by]][duplicated(cell_type_df[[group_by]])]
-  # if (length(duplicated_clusters) > 1) {
-  #   warning("Clusters: ", paste(as.character(duplicated_clusters), 
-  #                               collapse = ", "), " are annotated to more than one cell type. These labels will be concatenated in the final annotation.")
-  # }
-  # else if (length(duplicated_clusters) > 0) {
-  #   warning("Cluster ", as.character(duplicated_clusters), 
-  #           " is annotated to more than one cell type. These labels will be concatenated in the final annotation.")
-  # }
-  # cell_type_df <- aggregate(data = cell_type_df, tissue ~ ., FUN = paste, collapse = "/")
-  
   # add cell_type to seurat metadata
   seurat_obj@meta.data[[annotation_name]] <- unlist(apply(seurat_obj@meta.data, 
-                                                   1, function(x) ifelse(x[[group_by]] %in% cell_type_df[[group_by]], 
-                                                                         cell_type_df[cell_type_df[[group_by]] == x[[group_by]], "variable"], NA)))
+                                                          1, function(x) ifelse(x[[group_by]] %in% cell_type_df[[group_by]], 
+                                                                                cell_type_df[cell_type_df[[group_by]] == x[[group_by]], "variable"], NA)))
   return(seurat_obj)
 }
-
-
-
-
-
-
-
-
-
-
-
-
