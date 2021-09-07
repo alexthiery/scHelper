23ce9696bd15a081f0e4a5662ec4a7d2b922e58b
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-09-07 12:27:10 +0100

    overhaul state classification

--- R/ClusterClassification.R
+++ R/ClusterClassification.R
@@ -21,7 +21,7 @@ ClusterClassification <- function(seurat_obj = seurat_data, group_by = "seurat_c
   if (!length(quantile) == 1) {
     stop("\nquantile must be a single numeric value\n")
   }
-
+  
   iterate = TRUE
   iteration = 1
   metrics = names(cell_type_markers)
@@ -32,112 +32,109 @@ ClusterClassification <- function(seurat_obj = seurat_data, group_by = "seurat_c
     # Calculate average module expression for contamination gene list
     temp_seurat <- AverageGeneModules(seurat_obj = temp_seurat, gene_list = cell_type_markers)
     
-    # select clusters and cell type annotations which are higher than percentile threshold
-    celltype_df <- temp_seurat@meta.data %>%
-      dplyr::select(c(!!as.symbol(group_by), all_of(metrics))) %>%
-      reshape2::melt() %>% 
-      mutate(variable = as.character(variable)) %>% 
-      # Set threshold based on predefined percentile
-      group_by(variable, .drop = FALSE) %>%
-      mutate(threshold = quantile(value, probs = quantile)) %>%
-      group_by(!!as.symbol(group_by), variable, .drop = FALSE) %>%
-      mutate(median = median(value)) %>%
-      # Calculate the percentile for the median value of each cluster
-      group_by(variable, .drop = FALSE) %>%
-      mutate(median_percentile = ecdf(value)(median)) %>%
-      select(!value) %>%
-      distinct(.keep_all = TRUE)
-    
-    # select clusters and cell type annotations which are higher than percentile threshold
-    threshold_subset <- celltype_df %>%
-      # Filter clusters where the median is lower than the threshold value
-      filter(median >= threshold) %>%
-      # If cluster passes the threshold - assign to the cell-type with highest median percentile
-      group_by(!!as.symbol(group_by)) %>%
-      top_n(1, median_percentile) %>%
-      dplyr::select(!!as.symbol(group_by), variable)
-
     png(paste0(plot_path, "classification_iteration_", iteration, ".png"), width = 40, height = 30, units = "cm", res = 200)
     seurat_obj@misc[[annotation_name]] <- append(seurat_obj@misc[[annotation_name]], list( iteration = grid.arrange(grobs = PlotOutliers(seurat_obj = temp_seurat, 
                                                                                                                                          y_elements = metrics, group_by = group_by, 
                                                                                                                                          quantiles = quantile), ...)))
     graphics.off()
-
-    # if no cells are assigned, assign remaining clusters to highest percentile
-    if(nrow(threshold_subset) == 0){
-      iterate = FALSE
-      
-      # Filter clusters which dont pass threshold
-      below_threshold <- celltype_df %>%
-        filter(median < threshold) %>%
-        group_by(!!as.symbol(group_by)) %>%
-        top_n(1, median_percentile) %>%
-        dplyr::select(!!as.symbol(group_by), variable)
-        
-      if(!force_assign){
-        below_threshold$variable <- NA
-        cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned N/A. \nClusters:", 
-            as.character(unique(below_threshold[[group_by]])), '\n')
-      } else {
-        cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned to the cell type with the highest percentile. \nClusters:", 
-            as.character(unique(below_threshold[[group_by]])), '\n')
-      }
-      
-      threshold_subset <- rbind(threshold_subset, below_threshold)
-      
-    } 
-    # if not all cells are classified, remove assigned cells from seurat object in order to re-assign remaining cells
-    else if (!all(temp_seurat@meta.data[[group_by]] %in% threshold_subset[[group_by]])){
-      temp_seurat <- subset(temp_seurat, subset = !!as.symbol(group_by) %in% threshold_subset[[group_by]], invert=TRUE)
-      
-      # If only one cluster remains - assign as cannot calculate percentile with only one group
-      remaining_clusters <- filter(celltype_df, !(!!as.symbol(group_by)) %in% threshold_subset[[group_by]])
-      
-      if(length(unique(remaining_clusters[[group_by]])) == 1){
-        iterate = FALSE
-        cat('\nThe following clusters did not pass the percentile threshold. Clusters will be assigned to the cell type with the highest percentile. \nClusters:', as.character(unique(remaining_clusters[[group_by]])), '\n')
-        
-        threshold_subset <- remaining_clusters %>%
-          ungroup() %>%
-          top_n(1, median_percentile) %>%
-          dplyr::select(!!as.symbol(group_by), variable) %>%
-          rbind(threshold_subset)
-      }
-    } else {
-      iterate = FALSE
-      cat("\nAll clusters classified!\n")
-    }
+    
+    # select clusters and cell type annotations which are higher than percentile threshold
+    celltype_df = temp_seurat@meta.data %>%
+      dplyr::select(c(!!as.symbol(group_by), all_of(metrics))) %>%
+      reshape2::melt() %>%
+      mutate(variable = as.character(variable)) 
+    
+    # Calculate threshold module scores for each gene module
+    celltype_df = celltype_df %>%
+      group_by(variable, .drop = FALSE) %>%
+      mutate(threshold = quantile(value, probs = quantile))
+    
+    # Add median gene module scores for each cell cluster
+    celltype_df = celltype_df %>%
+      group_by(!!as.symbol(group_by), variable, .drop = FALSE) %>%
+      mutate(median = median(value)) %>%
+      group_by(variable, .drop = FALSE) %>%
+      mutate(median_percentile = ecdf(value)(median))
+    
+    # Drop individual cell gene module scores and remove duplicate rows
+    celltype_df = celltype_df %>%
+      select(!value) %>%
+      distinct(.keep_all = TRUE)
+    
+    # Select top classification for each cluster
+    celltype_df = celltype_df %>%
+      group_by(!!as.symbol(group_by)) %>% 
+      top_n(1, median_percentile)
+    
+    # Select clusters which pass the threshold subset
+    threshold_subset = filter(celltype_df, median >= threshold)
+    
+    # Select remaining (unclassified) clusters
+    remaining_clusters <- filter(celltype_df, !(!!as.symbol(group_by)) %in% threshold_subset[[group_by]])
+    
     
     # iteratively add cluster assignments
-    cell_type_df <- rbind(cell_type_df, threshold_subset)
-    iteration = iteration + 1
+    if(nrow(threshold_subset) != 0){
+      cell_type_df <- rbind(cell_type_df, threshold_subset)
+      
+      if(nrow(remaining_clusters) == 0){
+        iterate = FALSE
+        cat("\nAll clusters classified!\n")
+        
+      } else if (nrow(remaining_clusters) <= 1) {
+        iterate = FALSE
+        
+        if(!force_assign){
+          remaining_clusters$variable <- NA
+          cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned N/A. \nClusters:", 
+              as.character(unique(remaining_clusters[[group_by]])), '\n')
+        } else {
+          cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned to the cell type with the highest percentile. \nClusters:", 
+              as.character(unique(remaining_clusters[[group_by]])), '\n')
+        }
+        cell_type_df <- rbind(cell_type_df, remaining_clusters)
+        
+      } else{
+        iteration = iteration + 1
+        # Remove assigned cell types from temp_seurat object for next round of iteration
+        temp_seurat <- subset(temp_seurat, subset = !!as.symbol(group_by) %in% threshold_subset[[group_by]], invert=TRUE)
+      }
+      
+    } else if(nrow(threshold_subset) == 0){
+      iterate = FALSE
+      
+      if(!force_assign){
+        remaining_clusters$variable <- NA
+        cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned N/A. \nClusters:", 
+            as.character(unique(remaining_clusters[[group_by]])), '\n')
+      } else {
+        cat("\nThe following clusters did not pass the percentile threshold. Clusters will be assigned to the cell type with the highest percentile. \nClusters:", 
+            as.character(unique(remaining_clusters[[group_by]])), '\n')
+      }
+      cell_type_df <- rbind(cell_type_df, remaining_clusters)
+    }
   }
-  
-  if (nrow(cell_type_df) == 0) {
-    stop("\nNo cell types detected!\n")
-  }
-
-  # check if any clusters are annotated to multiple cell states (annotations share same percentile)
-  if(any(cell_type_df %>% count(seurat_clusters) %>% pull(n) > 1)){
-    duplicate_clusters <- cell_type_df %>%
-      count(seurat_clusters) %>%
-      filter(!n == 1) %>%
-      pull(seurat_clusters) %>%
-      as.character()
     
-    cat("\nFollowing clusters are equally annotated to multiple cell states. Annotated cell states will be concatenated. \nClusters:", duplicate_clusters, "\n")
+    # check if any clusters are annotated to multiple cell states (annotations share same percentile)
+    if(any(cell_type_df %>% count(seurat_clusters) %>% pull(n) > 1)){
+      duplicate_clusters <- cell_type_df %>%
+        count(seurat_clusters) %>%
+        filter(!n == 1) %>%
+        pull(seurat_clusters) %>%
+        as.character()
+      
+      cat("\nFollowing clusters are equally annotated to multiple cell states. Annotated cell states will be concatenated. \nClusters:", duplicate_clusters, "\n")
+      
+      cell_type_df <- cell_type_df %>%
+        group_by(seurat_clusters) %>%
+        mutate(variable = paste0(variable, collapse = "/")) %>%
+        distinct()
+    }
     
-    cell_type_df <- cell_type_df %>%
-      group_by(seurat_clusters) %>%
-      mutate(variable = paste0(variable, collapse = "/")) %>%
-      distinct()
+    # add cell_type to seurat metadata
+    seurat_obj@meta.data[[annotation_name]] <- unlist(apply(seurat_obj@meta.data, 
+                                                            1, function(x) ifelse(x[[group_by]] %in% cell_type_df[[group_by]], 
+                                                                                  cell_type_df[cell_type_df[[group_by]] == x[[group_by]], "variable"], NA)))
+    return(seurat_obj)
   }
-  
-  # add cell_type to seurat metadata
-  seurat_obj@meta.data[[annotation_name]] <- unlist(apply(seurat_obj@meta.data, 
-                                                          1, function(x) ifelse(x[[group_by]] %in% cell_type_df[[group_by]], 
-                                                                                cell_type_df[cell_type_df[[group_by]] == x[[group_by]], "variable"], NA)))
-  return(seurat_obj)
-}
-
 
